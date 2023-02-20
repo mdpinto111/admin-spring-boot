c85e8bdf1453132936c0df230057296d9a90523a
mdpinto111 <46295248+mdpinto111@users.noreply.github.com>
2023-02-20 16:22:14 +0200

    Add files via upload

---
+++ admin-spring-boot/src/main/java/com/example/admin/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.example.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableAdminServer
+
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
